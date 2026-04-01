ea3f844a37932738a2d92acb641ba566219cc4b4
Yuya.Nishida. <yuya@j96.org>
2026-04-02 00:57:02 +0900

    chore: add

---
+++ endless_ruby.rb
@@ -0,0 +1,14 @@
+class Foo
+  def bar
+    p([__FILE__, __LINE__])
+    if true
+      p([__FILE__, __LINE__])
+    else
+      p([__FILE__, __LINE__])
+    p([__FILE__, __LINE__])
+
+Foo.new.bar
+if false
+  p([__FILE__, __LINE__])
+else
+  p([__FILE__, __LINE__])
